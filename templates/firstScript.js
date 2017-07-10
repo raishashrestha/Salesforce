3fa3646d0b54c0a242055edbebe291f20c53e0fb
Raisha <engineer.rashu@gmail.com>
2017-07-10 16:10:16 +0545

    New commit

---
+++ templates/firstScript.js
@@ -0,0 +1,14 @@
+///reference path=<"/home/raisha/PycharmProjects/untitled2/templates/firstScript.js/>
+
+
+var myApp= angular.module("myModule",[]);
+
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.message= "Angular JS Tutorial";
+    $scope.firstname = "raisha";
+
+
+} );
+
